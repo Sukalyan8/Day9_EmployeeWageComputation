0618bf3f1579787e216c393d65c82f23d3c6f11a
Sukalyan <sukalyandash1920@gmail.com>
2022-01-14 17:17:05 +0530

    Multiple Company

---
+++ UC8_MultipleCompany.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9_EmployeeWageComputation
+{
+    interface UC8_MultipleCompany
+    {
+        public int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int MaxHoursPerMonth);
+    }
+}
